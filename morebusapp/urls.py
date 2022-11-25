8d5ccfb47b3d1605450e8b298b8a2175843e2f61
karunvee <you@example.com>
2022-11-25 14:38:47 +0700

    update_url_to_path

--- morebusapp/urls.py
+++ morebusapp/urls.py
@@ -3,10 +3,16 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_view, name='home_view'),
-    url(r'^line_view/ln(?P<ln>[0-9]+)/$', views.line_view, name='line_view'),
-    url(r'^machine_view/ln(?P<ln>[0-9]+)id(?P<id>[0-9]+)/$', views.item_view, name='item_view'),
-    url(r'setting/$', views.setting_view, name='setting_view'),
-    url(r'remote/$', views.remote_view, name='remote_view'),
-    url(r'delete_indicator/ln(?P<ln>[0-9]+)id(?P<id>[0-9]+)tag_id(?P<tid>[0-9]+)/$', views.delete_indicator, name='delete_indicator'),
+    # url(r'^$', views.home_view, name='home_view'),
+    # url(r'^line_view/ln(?P<ln>[0-9]+)/$', views.line_view, name='line_view'),
+    # url(r'^machine_view/ln(?P<ln>[0-9]+)id(?P<id>[0-9]+)/$', views.item_view, name='item_view'),
+    # url(r'setting/$', views.setting_view, name='setting_view'),
+    # url(r'remote/$', views.remote_view, name='remote_view'),
+    # url(r'delete_indicator/ln(?P<ln>[0-9]+)id(?P<id>[0-9]+)tag_id(?P<tid>[0-9]+)/$', views.delete_indicator, name='delete_indicator'),
+    path('', views.home_view, name='home_view'),
+    path('line_view/ln<int:ln>/', views.line_view, name='line_view'),
+    path('machine_view/ln<int:ln>id<int:id>/', views.item_view, name='item_view'),
+    path('setting/', views.setting_view, name='setting_view'),
+    path('remote/', views.remote_view, name='remote_view'),
+    path('delete_indicator/ln<int:ln>id<int:id>tag_id<int:tid>/', views.delete_indicator, name='delete_indicator'),
 ]
